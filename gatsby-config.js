306ee761eb11a69bd62d74bd2cce180b1a1794e9
Tofik Khedawala <toufiq@perceptionsystem.in>
2022-09-21 16:32:32 +0530

    change graph url

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `http://localhost/gatsby/graphql`,
+          `https://startergatsbymaster84172.gatsbyjs.io/graphql`,
       },
     },
 
