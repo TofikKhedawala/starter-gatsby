61ea20518573cd7ff049dac8031291d72831bb04
Tofik Khedawala <toufiq@perceptionsystem.in>
2022-09-21 16:40:00 +0530

    revert changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://startergatsbymaster84172.gatsbyjs.io/graphql`,
+          `http://localhost/gatsby/graphql`,
       },
     },
 
